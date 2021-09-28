b54dfe2a357245467ba3055dc4ebfa6b8d3c00ce
Takumikimura141899 <kimura141899@gmail.com>
2021-09-28 21:24:57 +0900

    [add] Facility_materialui_textfield_style

--- src/components/Facility.tsx
+++ src/components/Facility.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Container, Paper, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}));
 
 export const Facility: React.FC = () => {
-  return <div>Hello Facility!</div>;
+  const style = useStyle();
+  return (
+    <Container maxWidth="sm" className={style.root}>
+      <Paper className={style.paper}>
+        <TextField label="役職名" fullWidth />
+        <TextField label="詳細" fullWidth multiline />
+      </Paper>
+    </Container>
+  );
 };
