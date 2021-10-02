c86bdf9e607cb962500195aa8d3081d312dbe5c3
Takumikimura141899 <kimura141899@gmail.com>
2021-10-02 15:00:34 +0900

    [add] router

---
+++ src/components/Router.tsx
@@ -0,0 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
+
+import { Reservation } from './Reservation';
+import { Facility } from './Facility';
+import { ReservationList } from './ReservationList';
+
+export const Router: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/reservation" component={Reservation} />
+      <Route path="/facility" component={Facility} />
+      <Route path="/" exact component={ReservationList} />
+    </Switch>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Reservation } from './components/Reservation';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import Utils from '@date-io/dayjs';
 import 'dayjs/locale/ja';
 import { FaceTwoTone } from '@material-ui/icons';
 import { Dayjs } from 'dayjs';
+import { Router } from './components/Router';
 
 class ExtendedUtils extends Utils {
   getCalendarHeaderText(date: Dayjs) {
@@ -18,7 +20,9 @@ class ExtendedUtils extends Utils {
 
 ReactDom.render(
   <MuiPickersUtilsProvider utils={ExtendedUtils} locale="ja">
-    <Reservation />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </MuiPickersUtilsProvider>,
   document.getElementById('container'),
 );
